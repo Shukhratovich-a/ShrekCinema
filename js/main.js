427a37f05adaaa26e6b21207b96ee22a061dbf37
Shukhratovich-a <shukhratovich138@gmail.com>
2022-01-20 18:44:57 +0500

    Add date input

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var elDashboardInputName = document.querySelector(".dashboard__input[name='name'
 var elDashboardInputOverview = document.querySelector(".dashboard__input[name='overview']");
 var elDashboardInputGenres = document.querySelector(".dashboard__input[name='genres']");
 var elDashboardInputPoster = document.querySelector(".dashboard__input[name='poster']");
+var elDashboardInputDate = document.querySelector(".dashboard__input[name='date']");
 
 // Normalizing Date
 function normalizeData(setData) {
@@ -33,11 +34,13 @@ function renderMovies(moviesArray = [], moviesList) {
     var movieGenresList = document.createElement("ul");
 
     var releaseDate = new Date(moviesArray[i].release_date).getFullYear();
+    var releaseDate = new Date(moviesArray[i].release_date).getFullYear();
 
     // Set Attributes
     movie.setAttribute("class", "movie");
     moviePoster.setAttribute("class", "movie__poster");
     moviePoster.setAttribute("src", moviesArray[i].poster);
+    moviePoster.setAttribute("alt", moviesArray[i].title + "'s poster");
     moviePoster.setAttribute("alt", moviesArray[i].title + "'s poster");
     moviePoster.setAttribute("width", "280");
     moviePoster.setAttribute("height", "420");
@@ -45,6 +48,8 @@ function renderMovies(moviesArray = [], moviesList) {
     movieTitle.setAttribute("class", "movie__title");
     movieData.setAttribute("class", "movie__data");
     movieData.setAttribute("datatime", normalizeData(moviesArray[i].release_date));
+    movieData.setAttribute("datatime", normalizeData(moviesArray[i].release_date));
+    movieData.setAttribute("title", normalizeData(moviesArray[i].release_date));
     movieData.setAttribute("title", normalizeData(moviesArray[i].release_date));
     movieOverview.setAttribute("class", "movie__overview");
     movieGenresList.setAttribute("class", "movie__genres");
@@ -74,17 +79,19 @@ function renderMovies(moviesArray = [], moviesList) {
 }
 
 // Add New Movie 
-function addNewMovie(name, overview, genres, poster, moviesArray) {
+function addNewMovie(name, overview, genres, poster,date, moviesArray) {
   var newMovieName = name.value.trim();
   var newMovieOverview = overview.value.trim();
   var newMovieGenres = genres.value.trim().split(" ");
   var newMoviePoster = poster.value.trim();
+  var newMovieDate = date.value;
 
   var newMovie = {
     title: newMovieName,
     poster: newMoviePoster,
     overview: newMovieOverview,
     genres: newMovieGenres,
+    release_date: newMovieDate
   };
 
   moviesArray.unshift(newMovie);
@@ -98,6 +105,7 @@ var renderNewMovie = function (evt) {
     elDashboardInputOverview,
     elDashboardInputGenres,
     elDashboardInputPoster,
+    elDashboardInputDate,
     films
   );
 
